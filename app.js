9af0e41daae916f6016849678744af3d4c0a0b09
Ifra01 <jamalifra74@gmail.com>
2026-04-17 17:36:54 +0500

    proceeds to complete

--- app.js
+++ app.js
@@ -10,3 +10,17 @@ iphone10.addEventListener("click", () => {
     window.location.href = "mobile-products/iphone-10.html"
 })
 
+let iphone11 = document.querySelector("#iphone11");
+iphone11.addEventListener("click", () => {
+    window.location.href = "mobile-products/iphone-11.html"
+})
+
+let iphone8 = document.querySelector("#iphone8");
+iphone8.addEventListener("click", () => {
+    window.location.href = "mobile-products/iphone-8.html"
+})
+
+let iphone13 = document.querySelector("#iphone13");
+iphone13.addEventListener("click", () => {
+    window.location.href = "mobile-products/iphone-13.html"
+})
